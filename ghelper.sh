312657a02b0dca164f15898c99c9f95f09abd8aa
KyanJeuring <kyanjeuring@gmail.com>
2026-08-26 11:42:04 +0200

    Updated version number

--- ghelper.sh
+++ ghelper.sh
@@ -11,7 +11,7 @@ if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
   IFS=$'\n\t'
 fi
 
-GHELPER_VERSION="v1.3.1"
+GHELPER_VERSION="v1.4.0"
 
 # ==================================================
 # Configuration
